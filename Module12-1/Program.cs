58366f28b14a20fdc2707b8750bf6b45e374c785
Albina-K <kamalieva.albish@mail.ru>
2024-05-18 22:40:46 +0300

    process

--- Module12-1/Program.cs
+++ Module12-1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.InteropServices;
+﻿using BenchmarkDotNet.Running;
+using Module12_1;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Threading.Channels;
 
 namespace Module12
@@ -7,6 +10,14 @@ namespace Module12
     {
         static void Main(string[] args)
         {
+
+            Estimate(20);
+            Console.ReadKey();
+
+
+           /* var summary = BenchmarkRunner.Run<Testing>();
+            
+            
             Console.WriteLine("Как тебя зовут");
             var name = Console.ReadLine();
             var greetings = "Привет," + name;
@@ -37,17 +48,121 @@ namespace Module12
                 Console.WriteLine("Пользователи младше 14 лет не могут быть зарегестрированы");
             }
 
-
+            List<User> list = new List<User>() 
+            { 
+                new User ("AAA", "aaa", false),
+                new User ("BBB", "bbb", true),
+                new User ("CCC", "ccc", false)
+            };
               
-                
-                var premium = new User();
-
-                Console.WriteLine("Hello," + premium.Name);
-                if (premium.IsPremium != true)
+            foreach (var item in list)
+            {
+                Console.WriteLine("Hello," + item.Name);
+                if (item.IsPremium != true)
                 {
                     ShowAds();
                 }
-            
+            }
+
+            static void ShowAds()
+            {
+                Console.WriteLine("Посетите наш сайт с бесплаьными играми ...");
+                // Остановка на 1 с
+                Thread.Sleep(1000);
+
+                Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+                // Остановка на 2 с
+                Thread.Sleep(2000);
+
+                Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+                // Остановка на 2 с
+                Thread.Sleep(3000);
+            }
+
+
+            //бинанрный поиск с рекурсией
+            static int BinarySearch(int value, int[] array, int left, int right)
+            {
+
+                var middle = (left + right) / 2;
+
+                var midElement = array[middle];
+
+                if (midElement == value)
+                {
+                    return middle;
+                }
+                else if (left < right)
+                {
+                    if (value < midElement)
+                    {
+                        return BinarySearch(value, array, left, middle - 1);
+                    }
+                    else
+                    {
+                        return BinarySearch(value, array, middle + 1, right);
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            //бинарный поиск без рекурсии
+            static int BinarySearch2(int value, int[] array, int left, int right)
+            {
+                while (left <= right)
+                {
+                    var middle = (left + right) / 2;
+
+                    var midElement = array[middle];
+
+                    if (midElement == value)
+                    {
+                        return middle;
+                    }
+                    else if (value < midElement)
+                    {
+                        right = middle - 1;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
+                }
+
+                return -1;
+            }
+            //пузыриковая сортировка
+            static void BubbleSort(int[] array)
+            {
+                int temp;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[i] > array[j])
+                        {
+                            temp = array[i];
+                            array[i] = array[j];
+                            array[j] = temp;
+                        }
+                    }
+                }
+            }
+            //метод, который будет возвращать индекс элемента в отсортированном массиве, на место которого можно будет вставить элемент так, чтобы не нарушить порядок сортировки.
+            static int GetIndex(int[] array, int element)
+            {
+                int index;
+                for (index = 0; index < array.Length; index++)
+                {
+                    if (array[index] > element)
+                        return index;
+                }
+
+                return index;
+            }
+
 
         }
 
@@ -56,23 +171,41 @@ namespace Module12
             public string Login { get; set; }
             public string Name { get; set; }
             public bool IsPremium { get; set; }
-        static void ShowAds()
+
+            public User(string login, string name, bool isPremium)
+            {
+                Login = login;
+                Name = name;
+                IsPremium = isPremium;
+            }*/
+
+
+        }
+
+        static void CreateMatrix(int n)
         {
-            Console.WriteLine("Посетите наш сайт с бесплаьными играми ...");
-            // Остановка на 1 с
-            Thread.Sleep(1000);
+            var matrix = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
 
-            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
-            // Остановка на 2 с
-            Thread.Sleep(2000);
-
-            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-            // Остановка на 2 с
-            Thread.Sleep(3000);
+            for (int i = 0;i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = i + j;
+                }
+            }
         }
+
+        static void Estimate(int n)
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                CreateMatrix(10000);
+            }
         }
-
-
     }
 
 }
---
+++ Module12-1/Testing.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module12_1
+{
+    /*public class Testing
+    {
+        [Benchmark]
+        public void CreateMatrix()
+        {
+            int n = 10000;
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
+
+            for (int i = 0;i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = i + j;
+                }
+            }
+        }
+    }*/
+}
